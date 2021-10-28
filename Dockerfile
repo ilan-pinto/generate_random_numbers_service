8c2d78a7aab7d557f0fe10e70314cda6871dc080
Ilona Shishov <Ilona.Shishov@gmail.com>
2021-10-28 15:39:05 +0300

    added docker compose

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM openjdk:11.0.13-jre
+COPY  target/random-0.0.1-SNAPSHOT.jar .
+
+
+EXPOSE 8080
+
+CMD ["/bin/bash", "-c", "java -jar ./random-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/numbers/random/AccessingDataMysqlApplication.java
+++ src/main/java/com/numbers/random/AccessingDataMysqlApplication.java
@@ -3,7 +3,7 @@ package com.numbers.random;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class AccessingDataMysqlApplication {
     public static void main(String[] args) {
         SpringApplication.run(AccessingDataMysqlApplication.class, args);

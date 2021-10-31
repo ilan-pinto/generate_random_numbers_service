51d85fb1aa5eee23172a1e57cae8d06562e96fa3
root <ilona.shishov@gmail.com>
2021-10-31 12:38:42 +0200

    containerized applications

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM openjdk:11.0.13-jre
 COPY  target/random-0.0.1-SNAPSHOT.jar .
 
-
 EXPOSE 8080
 
 CMD ["/bin/bash", "-c", "java -jar ./random-0.0.1-SNAPSHOT.jar"]
--- src/main/java/com/numbers/random/RandomNumber.java
+++ src/main/java/com/numbers/random/RandomNumber.java
@@ -1,11 +1,9 @@
 package com.numbers.random;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "random_number")
 public class RandomNumber {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
